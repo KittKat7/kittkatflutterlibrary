6ec04bbf051b6497f836eb85a9a4d3cc68a97939
kittkat7 <rainbowtabby7@gmail.com>
2024-01-20 13:46:02 -0500

    Updates to AppTheme

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -31,21 +31,60 @@ List<MaterialColor> getAvailableColors() {
 
 
 class AppTheme extends ChangeNotifier {
-  MaterialColor themeColor;
-  ThemeMode themeMode;
-  ThemeData themeData;
+  MaterialColor _themeColor;
+  ThemeMode _themeMode;
+  ThemeData _themeData;
+  ThemeData get themeData => _themeData;
 
   // ThemeMode mode = Theme.of(context).brightness;
 
-  AppTheme({required this.themeMode, required this.themeColor})
-  : themeData = ThemeData(
+  AppTheme({required ThemeMode themeMode, required MaterialColor themeColor})
+  : _themeMode = themeMode, _themeColor = themeColor, _themeData = ThemeData(
     brightness: Brightness.light,
     primarySwatch: themeColor,
   );
 
-  ThemeData getTheme() {
-    return themeData;
-  }
+  /// Builds the theme with the given settings.
+  /// 
+  /// Uses the theme comonents, color, and mode, and sets [themeData] to reflect those factors.
+  /// Then calls notifyListener() to update anything using the theme.
+  void buildAppTheme() {
+    Brightness brightness = Brightness.light;
+    // The brightness used when building the theme.
+    // Determine the brightness. // TODO
+    if (_themeMode == ThemeMode.dark) {
+      brightness = Brightness.dark;
+    }//e if
+
+    // Update [_themeData] to reflect any changes to the components.
+    _themeData = ThemeData(
+      brightness: brightness,
+      primarySwatch: _themeColor,
+    );
+    // Notify the listeners.
+    notifyListeners();
+  }//e buildThemeData
+
+  /// Cycles the current color to the next color in the list of available colors.
+  /// 
+  /// Changes the theme color to the next color in the list of available colors. If the current
+  /// color is NOT in the list, then it resest to the begening of the list.
+  void cylceColor() {
+    // Get the list of available colors.
+    List<MaterialColor> colors = getAvailableColors();
+    // Determine the index of the current color and add 1 (for the index of the next color). If [i]
+    // is outside of the bounds of the list, reset to 0. If the current color is not in the list,
+    // [i] will be set to (-1 + 1) which is 0, and the new color will be from the start of the list.
+    int i = colors.indexOf(_themeColor) + 1;
+    // If [i] is out of bounds, use color 0, otherwise use color [i].
+    if (i >= colors.length || i < 0) {
+      _themeColor = colors[0];
+    } else {
+      _themeColor = colors[i];
+    }//e if else
+    // Build the new theme.
+    buildAppTheme();
+  }//e cycleColors
 
   // Brightness _modeToBrightness() {
   //   if (themeMode == ThemeMode.light) {
