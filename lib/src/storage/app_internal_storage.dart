a25901d97cc711ac06989148e35aa1715bcc6614
kittkat7 <rainbowtabby7@gmail.com>
2024-01-22 16:17:55 -0500

    Small update to aoo internal storage

--- lib/src/storage/app_internal_storage.dart
+++ lib/src/storage/app_internal_storage.dart
@@ -10,41 +10,42 @@ import 'package:kittkatflutterlibrary/src/app_info.dart';
 import 'package:path_provider/path_provider.dart';
 import '../platform.dart';
 
-/// Abstract class. The `initiate()` method initiates the _AppDocuments to be ready to read/write
+/// Abstract class. The `initiate()` method initiates the _AppInternalFiles to be ready to read/write
 /// documents. `readAsString(String path)` reads the specified file as a string.
-abstract class _AppDocuments {
+abstract class _AppInternalFiles {
   /// `initiate()` is used to initiate any files that may require async.
-  Future<void> initiate();
+  Future<void> initialize();
   /// Reads a string from specified file.
-  Future<String?> readAppDoc(String path);
+  Future<String?> readString(String path);
   /// Writes to a string to the specified file.
-  Future<void> writeAppDoc(String path, String data);
-}// _AppDocuments
+  Future<void> writeString(String path, String data);
+}// _AppInternalFiles
 
-/// [AppDocuments] allows for reading and writing files from the application document directory on
+
+/// [AppInternalFiles] allows for reading and writing files from the application document directory on
 /// native apps, or from IndexedDB if on web.
-class AppDocuments implements _AppDocuments {
+class AppInternalFiles implements _AppInternalFiles {
 
-  /// The [_AppDocuments] instance that will be used.
-  late _AppDocuments _appDocuments;
-  /// Whether [_appDocuments] has been initialized.
+  /// The [_AppInternalFiles] instance that will be used.
+  late _AppInternalFiles _appInternalFiles;
+  /// Whether [_appInternalFiles] has been initialized.
   bool isInitiated = false;
 
-  /// Initialize [_appDocuments] to be [_IndexedDBDocuments] if web, or [_NativeAppDocuments] if
+  /// Initialize [_appInternalFiles] to be [_IndexedDBInternalFiles] if web, or [_NativeAppInternalFiles] if
   /// native. After, set [isInitiated] to true.
   @override
-  Future<void> initiate() async {
-    _appDocuments = AppPlatform.isWeb? _IndexedDBDocuments() : _NativeAppDocuments();
-    await _appDocuments.initiate();
+  Future<void> initialize() async {
+    _appInternalFiles = AppPlatform.isWeb? _IndexedDBInternalFiles() : _NativeAppInternalFiles();
+    await _appInternalFiles.initialize();
     isInitiated = true;
   }// initiate
   
-  /// Reads the specified app document [path] from [_appDocuments].
+  /// Reads the specified app document [path] from [_appInternalFiles].
   @override
-  Future<String?> readAppDoc(String path) async {
-    if (!isInitiated) await initiate();
+  Future<String?> readString(String path) async {
+    if (!isInitiated) await initialize();
     try {
-      return await _appDocuments.readAppDoc(path);
+      return await _appInternalFiles.readString(path);
     } catch (e) {
       if (e is PathNotFoundException) return null;
     }
@@ -53,58 +54,60 @@ class AppDocuments implements _AppDocuments {
   
   /// Writes the specified content [data] to [path] as a String.
   @override
-  Future<void> writeAppDoc(String path, String data) async {
-    if (!isInitiated) await initiate();
-    await _appDocuments.writeAppDoc(path, data);
+  Future<void> writeString(String path, String data) async {
+    if (!isInitiated) await initialize();
+    await _appInternalFiles.writeString(path, data);
   }// writeAppDoc
-}// AppDocuments
+}// AppInternalFiles
 
-/// [_AppDocuments] for native apps.
-class _NativeAppDocuments implements _AppDocuments {
+
+/// [_AppInternalFiles] for native apps.
+class _NativeAppInternalFiles implements _AppInternalFiles {
 
   late String appDocumentPath;
 
   @override
-  Future<void> initiate() async {
-    appDocumentPath = AppPlatform.isWeb? '': (await getApplicationSupportDirectory()).path;
-  }// initNativeAppDocuments
+  Future<void> initialize() async {
+    appDocumentPath = (await getApplicationSupportDirectory()).path;
+  }// initiate
 
   @override
-  Future<String?> readAppDoc(String path) async {
+  Future<String?> readString(String path) async {
     return await _getFile(path).readAsString();
-  }
+  }// readAppDoc
 
   @override
-  Future<void> writeAppDoc(String path, String data) async {
+  Future<void> writeString(String path, String data) async {
     await _getFile(path).writeAsString(data);
-  }
+  }// writeAppDoc
 
   File _getFile(String path) {
     return File('$appDocumentPath/$path');
-  }
+  }// _getFile
   
 
-}// _AppDocuments
+}// _AppInternalFiles
 
-/// [_AppDocuments] for web apps.
-class _IndexedDBDocuments implements _AppDocuments {
+
+/// [_AppInternalFiles] for web apps.
+class _IndexedDBInternalFiles implements _AppInternalFiles {
 
   late Database _database;
   final String storeName = 'applicationSupportDirectory';
 
   @override
-  Future<void> initiate() async {
+  Future<void> initialize() async {
     _database = await idbFactoryBrowser.open(
       AppInformation.name,
       version: 1,
       onUpgradeNeeded: (VersionChangeEvent e) {
         final db = e.database;
         db.createObjectStore(storeName);
-  });
+    });
   }// initiate
 
   @override
-  Future<String?> readAppDoc(String path) async {
+  Future<String?> readString(String path) async {
     Transaction transaction = _database.transaction(storeName, "readonly");
     ObjectStore store = transaction.objectStore(storeName);
     String value = await store.getObject(path) as String;
@@ -113,11 +116,11 @@ class _IndexedDBDocuments implements _AppDocuments {
   }// readAsString
 
   @override
-  Future<void> writeAppDoc(String path, String data) async {
+  Future<void> writeString(String path, String data) async {
     Transaction transaction = _database.transaction(storeName, "readwrite");
     ObjectStore store = transaction.objectStore(storeName);
     await store.put(data, path);
     await transaction.completed;
   }// writeAppDoc
-}// _IndexedDBDocuments
+}// _IndexedDBInternalFiles
 
